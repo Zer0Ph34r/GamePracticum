9577f11e4c2f90e101221d19db09c026af7bbdf4
Zer0Ph34r <sepheroth0504@yahoo.com>
2017-01-25 15:32:13 -0700

    Added Black .PNG Added black Material added GameControllerScript added global variables script

---
+++ Game Precticum Prototype/Assets/Scritps/Contollers/GameControllerScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControllerScript : MonoBehaviour {
+
+    #region Feilds
+
+    // Gem game objects
+    GameObject whiteGem;
+    GameObject redGem;
+    GameObject yellowGem;
+    GameObject greenGem;
+    GameObject blueGem;
+
+    // table size int X int
+    //int tableSize =
+
+    #endregion
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Game Precticum Prototype/Assets/Scritps/Contollers/GlobalVariablesScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalVariablesScript {
+
+    #region Feilds
+
+    public const int TABLE_SIZE = 15;
+    //public const int 
+
+    #endregion
+}
