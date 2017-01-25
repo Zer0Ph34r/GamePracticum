b8cec65798851dbc6c8b58c27489710e26d15d01
Zer0Ph34r <sepheroth0504@yahoo.com>
2017-01-25 15:06:41 -0700

    Added Menu Script Added Back BUtton Script Added Title Added scaling canvases

---
+++ Game Precticum Prototype/Assets/Scritps/Menus/MenuButtonScript.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuButtonScript : MonoBehaviour {
+
+    #region Fields
+
+    // other canvases
+    [SerializeField]
+    Canvas CreditsCanvas;
+    [SerializeField]
+    Canvas InstructionsCanvas;
+
+    // buttons on this canvas
+    [SerializeField]
+    Button startButton;
+    [SerializeField]
+    Button instructionsButton;
+    [SerializeField]
+    Button creditsButton;
+    [SerializeField]
+    Button quitButton;
+
+    #endregion
+
+    //private void Start()
+    //{
+    //    startButton = GetComponent<Button>();
+    //}
+
+    /// <summary>
+    /// Change scene to Main Scene
+    /// </summary>
+    public void StartButton()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+
+    /// <summary>
+    /// Open Tutoriel button
+    /// </summary>
+    public void TutorialButton()
+    {
+
+    }
+
+    /// <summary>
+    /// Open credits canvas and close button canvas
+    /// </summary>
+    public void CreditsButton()
+    {
+
+    }
+
+    /// <summary>
+    /// Exit game
+    /// </summary>
+    public void QuitButton()
+    {
+        Application.Quit();
+    }
+}
