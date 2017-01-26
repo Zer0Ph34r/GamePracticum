a5f42329df9ea03129ff8a3d063199a120a7108f
Zer0Ph34r <sepheroth0504@yahoo.com>
2017-01-26 14:48:11 -0700

    Moved Materials

--- Game Precticum Prototype/Assets/Scritps/Contollers/GameControllerScript.cs
+++ Game Precticum Prototype/Assets/Scritps/Contollers/GameControllerScript.cs
@@ -6,6 +6,9 @@ public class GameControllerScript : MonoBehaviour {
 
     #region Feilds
 
+    // Main Cemera
+    Camera mainCamera;
+
     // Gem game objects
     GameObject whiteGem;
     GameObject redGem;
@@ -17,6 +20,9 @@ public class GameControllerScript : MonoBehaviour {
     // table size int X int
     int tableSize = GlobalVariables.TABLE_SIZE;
 
+    // 2D array of table contents
+    GameObject[,] gems;
+
     #endregion
 
     // Use this for initialization
@@ -24,14 +30,26 @@ public class GameControllerScript : MonoBehaviour {
 
         #region Load Assets
         whiteGem = Resources.Load<GameObject>("Prefabs/PryamidWhite");
-        redGem = Resources.Load<GameObject>("Prefabs/PryamidRed");
-        yellowGem = Resources.Load<GameObject>("Prefabs/PryamidYelow");
+        redGem = Resources.Load<GameObject>("Prefabs/PyramidRed");
+        yellowGem = Resources.Load<GameObject>("Prefabs/PryamidYellow");
         greenGem = Resources.Load<GameObject>("Prefabs/PryamidGreen");
-        blueGem = Resources.Load<GameObject>("Prefabs/PryamidBlue");
-        purpleGem = Resources.Load<GameObject>("Prefabs/PryamidPurple");
+        blueGem = Resources.Load<GameObject>("Prefabs/PyramidBlue");
+        purpleGem = Resources.Load<GameObject>("Prefabs/PyramidPurple");
         #endregion
 
         #region Create Game Board
+        // create table
+        gems = new GameObject[tableSize, tableSize];
+        // fill table and create game board
+        CreateGameBoard();
+
+        #endregion
+
+        #region Set Camera
+        //get main camera
+        mainCamera = Camera.main;
+        // set camera's position according to table size
+        mainCamera.transform.position = new Vector3(tableSize, tableSize, tableSize);
 
         #endregion
 
@@ -41,4 +59,69 @@ public class GameControllerScript : MonoBehaviour {
     void Update () {
 		
 	}
+
+    #region Methods
+
+    /// <summary>
+    /// Creates game board according to game board size
+    /// </summary>
+    void CreateGameBoard()
+    {
+        for (int i = 0; i < tableSize; ++i)
+        {
+            for (int k = 0; k < tableSize; ++k)
+            {
+                GameObject go = (GameObject)Instantiate(RandomizeObject(), new Vector3(i, k, 0), Quaternion.identity);
+                gems[i, k] = go;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Returns random gem color to create
+    /// </summary>
+    /// <returns></returns>
+    GameObject RandomizeObject()
+    {
+        // return object
+        GameObject returnGem = new GameObject();
+        // radom number between 0 and number of gems
+        switch ((int)Random.Range(0,6))
+        {
+            case 0:
+                {
+                    returnGem = whiteGem;
+                    break;
+                }
+            case 1:
+                {
+                    returnGem = redGem;
+                    break;
+                }
+            case 2:
+                {
+                    returnGem = blueGem;
+                    break;
+                }
+            case 3:
+                {
+                    returnGem = greenGem;
+                    break;
+                }
+            case 4:
+                {
+                    returnGem = yellowGem;
+                    break;
+                }
+            case 5:
+                {
+                    returnGem = purpleGem;
+                    break;
+                }
+        }
+
+        return returnGem;
+    }
+
+    #endregion
 }
