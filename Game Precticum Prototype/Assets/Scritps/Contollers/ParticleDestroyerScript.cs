680da42f0a1de2fe39c9f379c1980116afc33253
Jordan <sepheroh0504@yahoo.com>
2017-02-22 14:10:13 -0700

    Added delete on end for particle system

---
+++ Game Precticum Prototype/Assets/Scritps/Contollers/ParticleDestroyerScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class ParticleDestroyerScript : MonoBehaviour {
+
+    ParticleSystem ps;
+
+    private void Start()
+    {
+        ps = GetComponent<ParticleSystem>();
+    }
+
+
+    void Update()
+    {
+        if (ps)
+        {
+            if (!ps.IsAlive())
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
